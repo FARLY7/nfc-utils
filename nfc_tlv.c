8f083cc5846bb43c909ffed095d1796436fc037c
Sean Farrelly <sean.farrelly@outlook.com>
2019-08-10 14:22:38 +0100

    First refactoring of imeplementation.

--- nfc_tlv.c
+++ nfc_tlv.c
@@ -1,5 +1,123 @@
 #include "nfc_tlv.h"
-#include "logger.h"
+
+
+
+/* How do you avoid malloc? */
+
+tlv_status_t tlv_parse(uint8_t *buf, size_t len)
+{
+    int8_t rslt;
+
+    tlv_t tlv[10];
+    uint8_t tlv_cnt = 0;
+
+    if((buf == NULL) || (len == 0))
+    {
+        rslt = TLV_E_INVALID_ARGS;
+        goto error;
+    } 
+
+    while (len--)
+    {
+        switch (*buf)
+        {
+            case TLV_TYPE_NULL:
+            {
+                tlv[tlv_cnt].type = *buf++;
+                tlv[tlv_cnt].length = 0;
+                tlv[tlv_cnt].value = 0;
+                break;
+            }
+            case TLV_TYPE_LOCK_CONTROL:
+            {
+                if (*buf != TLV_LEN_LOCK_CONTROL)
+                {
+                    rslt = TLV_E_FORMAT;
+                    goto error;
+                }
+
+                tlv[tlv_cnt].type = *buf++;
+                tlv[tlv_cnt].length = *buf++;
+                tlv[tlv_cnt].value = buf;
+
+                buf += tlv[tlv_cnt].length;
+                break;
+            }
+            case TLV_TYPE_MEM_CONTROL:
+            {
+                if (*buf != TLV_LEN_MEM_CONTROL)
+                {
+                    rslt = TLV_E_FORMAT;
+                    goto error;
+                }
+
+                tlv[tlv_cnt].type = *buf++;
+                tlv[tlv_cnt].length = *buf++;
+                tlv[tlv_cnt].value = buf;
+
+                buf += tlv[tlv_cnt].length;
+                break;
+            }
+            case TLV_TYPE_NDEF_MESSAGE:
+            {
+                tlv[tlv_cnt].type = *buf++;
+
+                if (*buf == 0xFF)
+                {
+                    /* Long record? */
+                    tlv[tlv_cnt].length = ((*(buf + 1) << 8) & 0xFF00) | ((*(buf + 2) << 0) & 0x00FF);
+                    buf += 3; /* Replace with #define */
+                }
+                else
+                {
+                    /* Short record */
+                    tlv[tlv_cnt].length = *buf++;
+                }
+
+                tlv->value  = buf++;
+                break;
+            }
+            case TLV_TYPE_PROPRIETARY:
+            {
+                tlv[tlv_cnt].type = *buf++;
+
+                if (*buf == 0xFF)
+                {
+                    /* Long record? */
+                    tlv[tlv_cnt].length = ((*(buf + 1) << 8) & 0xFF00) | ((*(buf + 2) << 0) & 0x00FF);
+                    buf += 3; /* Replace with #define */
+                }
+                else
+                {
+                    /* Short record */
+                    tlv[tlv_cnt].length = *buf++;
+                }
+
+                tlv->value  = buf++;
+                break;
+            }
+            case TLV_TYPE_TERMINATOR:
+            {
+                tlv[tlv_cnt].type = *buf++;
+                tlv[tlv_cnt].length = 0;
+                tlv[tlv_cnt].value = 0;
+
+                /* Now finished parsing */
+                rslt = TLV_OK;
+                goto error;
+            }
+            default:
+            {
+                rslt = TLV_E_FORMAT;
+                goto error;
+            }
+
+            tlv_cnt++;
+        }
+
+error:
+    return rslt;
+}
 
 TLV_StatusTypeDef NFC_TLV_ParseNDEF(uint8_t *buf, size_t len, TLV_TypeDef *tlv)
 {
@@ -8,96 +126,22 @@ TLV_StatusTypeDef NFC_TLV_ParseNDEF(uint8_t *buf, size_t len, TLV_TypeDef *tlv)
 
     //size_t tempLen = len;
 
-    while (len--)
-    {
-        switch (*buf++)
-        {
-        case TLV_TYPE_NULL:
-        {
-            /* Skip NULL TLV */
-            break;
-        }
 
-        case TLV_TYPE_LOCK_CONTROL:
-        {
-            if (*buf != TLV_LEN_LOCK_CONTROL) /* Length MUST = 0x3 */
-                return TLV_ERROR;
-            buf += TLV_LEN_LOCK_CONTROL; /* Skip Lock Control TLV */
-            break;
-        }
-        case TLV_TYPE_MEM_CONTROL:
-        {
-            if (*buf != TLV_LEN_MEM_CONTROL) /* Length MUST = 0x3 */
-                return TLV_ERROR;
-            buf += TLV_LEN_MEM_CONTROL; /* Skip Memory Control TLV */
-            break;
-        }
-        case TLV_TYPE_NDEF_MESSAGE:
-        {
-            if (*buf == 0xFF)
-            {
-                length = ((*(buf + 1) << 8) & 0xFF00) |
-                         ((*(buf + 2) << 0) & 0x00FF);
-                buf += 3;
-            }
-            else
-                length = *buf++;
-
-            /* Parse NDEF Message */
-            tlv->type   = TLV_TYPE_NDEF_MESSAGE;
-            tlv->length = length;
-            tlv->value  = buf;
-
-            return TLV_OK;
-
-            break;
-        }
-        case TLV_TYPE_PROPRIETARY:
-        {
-            if (*buf == 0xFF)
-            {
-                length = ((*(buf + 1) << 8) & 0xFF00) |
-                         ((*(buf + 2) << 0) & 0x00FF);
-                buf += 3;
-            }
-            else
-                length = *buf++;
-
-            buf += length; /* Skip Proprietary TLV */
-
-            break;
-        }
-        case TLV_TYPE_TERMINATOR:
-        {
-            /* Do nothing */
-            break;
-        }
-        default:
-        {
-            break;
-        }
-        }
     }
 
     return TLV_ERROR;
 }
 
-void NFC_TLV_PrintTLV(TLV_TypeDef *tlv)
-{
-    Log_info("===== TLV =====");
-
-    Log_info("Type: %02X", tlv->type);
-    Log_info("Length: %d", tlv->length);
-
-    Log_print("Value: ", 7);
-    //Log_println(tlv->value, tlv->length);
-    for(size_t i = 0 ; i < tlv->length ; i++)
-    {
-        Log_info("%c", tlv->value[i]);
-    }
-}
-// void NFC_TLV_WriteTLV(uint16_t address, uint16_t offset, TLV_TypeDef *tlv, uint32_t timeout)
+// void NFC_TLV_PrintTLV(TLV_TypeDef *tlv)
 // {
-
-//     NFC_WriteMemory(&hnfc, address, byteOffset, , timeout);
+//     Log_info("===== TLV =====");
+//     Log_info("Type: %02X", tlv->type);
+//     Log_info("Length: %d", tlv->length);
+//     Log_print("Value: ", 7);
+//     //Log_println(tlv->value, tlv->length);
+//     for(size_t i = 0 ; i < tlv->length ; i++)
+//     {
+//         Log_info("%c", tlv->value[i]);
+//     }
 // }
+
--- nfc_tlv.h
+++ nfc_tlv.h
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <stddef.h>
 
+/* TLV Types */
 #define TLV_TYPE_NULL               0x00U
 #define TLV_TYPE_LOCK_CONTROL       0x01U
 #define TLV_TYPE_MEM_CONTROL        0x02U
@@ -11,24 +12,27 @@
 #define TLV_TYPE_PROPRIETARY        0xFDU
 #define TLV_TYPE_TERMINATOR         0xFEU
 
+/* TLV lengths */
 #define TLV_LEN_LOCK_CONTROL        0x03U
 #define TLV_LEN_MEM_CONTROL         0x03U
 
 typedef enum {
-    TLV_OK              = 0U,
-    TLV_ERROR           = 1U,
-    TLV_INVALID_ARGS    = 2U
-} TLV_StatusTypeDef;
-
+    TLV_OK,
+    TLV_E_INVALID_ARGS,
+    TLV_E_FORMAT,
+} tlv_status_t;
 
 typedef struct {
     uint8_t  type;
     uint32_t length;
     uint8_t  *value;
-} TLV_TypeDef;
+} tlv_t;
 
 
-TLV_StatusTypeDef NFC_TLV_ParseNDEF(uint8_t *buf, size_t len, TLV_TypeDef *tlv);
-void NFC_TLV_PrintTLV(TLV_TypeDef *tlv);
+tlv_status_t tlv_parse(uint8_t *buf, size_t len);
+
+
+// TLV_StatusTypeDef NFC_TLV_ParseNDEF(uint8_t *buf, size_t len, TLV_TypeDef *tlv);
+// void NFC_TLV_PrintTLV(TLV_TypeDef *tlv);
 
 #endif
